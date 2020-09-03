96ca65f97368c0f35dd9d120ca296d7ca6e0b417
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-03 17:40:35 +0000

    Deploy ayarhlaine/airbnb-clone to github.com/ayarhlaine/airbnb-clone.git:gh-pages

---
+++ precache-manifest.521a0e8a9efa955edeb6bcfbd4d2de81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a546faabb5e32dce212cb4c97c17432d",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "7cc25a23c1d8339856e2",
+    "url": "/airbnb-clone/static/css/main.5ea9aaaa.chunk.css"
+  },
+  {
+    "revision": "46787a488d383d49813e",
+    "url": "/airbnb-clone/static/js/2.2084ceaf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/airbnb-clone/static/js/2.2084ceaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc25a23c1d8339856e2",
+    "url": "/airbnb-clone/static/js/main.1ce3ec64.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
